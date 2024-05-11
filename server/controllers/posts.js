f761321d2f8007da5cebfef68ccc6fd029027fb2
ntthuyvy73 <ntthuyvy73@gmail.com>
2024-05-11 15:00:08 +0700

    commit

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,17 +1,18 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
     try {
         const postMessage = await PostMessage.find();
 
-        console.log(postMessage);
+        //   console.log(postMessage);
         res.status(200).json(postMessage);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
 };
 
-export const createPosts = async (req, res) => {
+export const createPost = async (req, res) => {
     const post = req.body;
 
     const newPost = new PostMessage(post);
@@ -21,4 +22,57 @@ export const createPosts = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
+};
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+
+    // console.log(_id);
+
+    if (!mongoose.Types.ObjectId.isValid(_id))
+        return res.status(404).send(`No post with id: ${_id}`);
+
+    const post = req.body;
+
+    const updatePost = await PostMessage.findByIdAndUpdate(
+        _id,
+        { ...post, _id },
+        {
+            new: true,
+        }
+    );
+
+    res.json(updatePost);
+};
+
+export const deletePost = async (req, res) => {
+    const { id: _id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(_id))
+        return res.status(404).send(`No post with id: ${_id}`);
+
+    // console.log("deletePost ");
+    // console.log(_id);
+
+    await PostMessage.findByIdAndDelete(_id);
+    res.json({ message: "Deleted Successful!" });
+};
+
+export const likePost = async (req, res) => {
+    const { id: _id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(_id))
+        return res.status(404).send(`No post with id: ${_id}`);
+
+    const post = await PostMessage.findById(_id);
+    const updatePost = await PostMessage.findByIdAndUpdate(
+        _id,
+        {
+            likeCount: post.likeCount + 1,
+        },
+        {
+            new: true,
+        }
+    );
+    res.json(updatePost);
 };
--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import mongoose from "mongoose";
 import express from "express";
 import bodyParser from "body-parser";
@@ -12,17 +15,16 @@ app.use(cors());
 
 app.use("/posts", router);
 
-const CONNECTION_URL =
-    "mongodb+srv://thuyvy:123@cluster0.elbyxxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const CONNECTION_URL =
+//     "mongodb+srv://thuyvy:123@cluster0.elbyxxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-    .connect(CONNECTION_URL)
+    .connect(process.env.CONNECTION_URL)
     .then(() =>
         app.listen(PORT, () =>
             console.log(`Server Running on Port: http://localhost:${PORT}`)
         )
     )
     .catch((error) => console.log(`${error} did not connect`));
-
-// mongoose.set("useFindAndModify", false);
